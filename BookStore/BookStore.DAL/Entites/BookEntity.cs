0d36076d623c11f5f473e1230fe8a7e278a8de4e
Adelina Sharafutdinova <adelisha.dinov@gmail.com>
2025-02-27 16:29:47 +0300

    add: BookStore/Entites

--- BookStore/BookStore.DAL/Entites/BookEntity.cs
+++ BookStore/BookStore.DAL/Entites/BookEntity.cs
@@ -2,5 +2,8 @@ namespace BookStore.DAL.Entites;
 
 public class BookEntity
 {
-    
+    public Guid Id { get; set;  }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
 }

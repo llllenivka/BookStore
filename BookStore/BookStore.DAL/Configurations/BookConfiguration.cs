cd545f1c3d0570e23520bf2c93d345bbcff689fa
Adelina Sharafutdinova <adelisha.dinov@gmail.com>
2025-02-27 16:29:05 +0300

    add: Configuration

--- BookStore/BookStore.DAL/Configurations/BookConfiguration.cs
+++ BookStore/BookStore.DAL/Configurations/BookConfiguration.cs
@@ -1,6 +1,25 @@
+using BookStore.Core.Models;
+using BookStore.DAL.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace BookStore.DAL.Configurations;
 
-public class BookConfiguration
+public class BookConfiguration : IEntityTypeConfiguration<BookEntity>
 {
+    public void Configure(EntityTypeBuilder<BookEntity> builder)
+    {
+        builder.HasKey(b => b.Id);
+        
+        builder.Property(b => b.Title)
+            .HasMaxLength(Book.MAX_TITLE_LENGTH)
+            .IsRequired();
+
+        builder.Property(b => b.Description)
+            .IsRequired();
+        
+        builder.Property(b => b.Price)
+            .IsRequired();
+    }
     
 }

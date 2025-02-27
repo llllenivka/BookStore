c003c6a661742f22f55e0fc6d09ed1c281b48f90
Adelina Sharafutdinova <adelisha.dinov@gmail.com>
2025-02-27 15:23:03 +0300

    add: BookStore.Core/Models

--- BookStore/BookStore.Core/Models/Book.cs
+++ BookStore/BookStore.Core/Models/Book.cs
@@ -2,5 +2,30 @@ namespace BookStore.Core.Models;
 
 public class Book
 {
+    private const int MAX_TITLE_LENGTH = 250;
+    private Book(Guid id, string title, string description, decimal price)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Price = price;
+    }
+    public Guid Id { get; }
+    public string Title { get; } = string.Empty;
+    public string Description { get; } = string.Empty;
+    public decimal Price { get; }
+
+    public static (Book book, string error) Create(Guid id, string title, string description, decimal price)
+    {
+        var error = string.Empty;
+        if (string.IsNullOrEmpty(title) || title.Length > MAX_TITLE_LENGTH)
+        {
+            error = "Title can not be empty or more than " + MAX_TITLE_LENGTH + " characters.";
+        }
+        
+        var book = new Book(id, title, description, price);
+        return (book, error);
+    }
+    
     
 }

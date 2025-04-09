26e223bab66bd6dc0de7b09fae8f2efa78c2f242
Adelina Sharafutdinova <adelisha.dinov@gmail.com>
2025-04-09 16:20:56 +0300

    add: CRUD

--- src/BookStore/BookStore.Api/Controllers/BookController.cs
+++ src/BookStore/BookStore.Api/Controllers/BookController.cs
@@ -15,10 +15,45 @@ public class BookController : ControllerBase
         _bookService = bookService;
     }
 
+    
     [HttpGet]
     public async Task<ActionResult<IEnumerable<BookResponseDto>>> GetAllBooks()
     {
         var books = await _bookService.GetAllBooksAsync();
         return Ok(books);
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<BookResponseDto>> GetBookById(Guid id)
+    {
+        var book = await _bookService.GetBookByIdAsync(id);
+        if (book == null) return NotFound();
+        return Ok(book);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<BookResponseDto>> CreateBook(BookRequestDto book)
+    {
+        var newBook = await _bookService.AddBookAsync(book);
+        return CreatedAtAction(nameof(GetBookById), new { id = newBook.Id }, newBook);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<BookResponseDto>> UpdateBook(Guid id, BookRequestDto book)
+    {
+        var updatedBook = await _bookService.UpdateBookAsync(id, book);
+        if (updatedBook == null) return NotFound();
+        return Ok(updatedBook);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<BookResponseDto>> DeleteBook(Guid id)
+    {
+        var deletedBook = await _bookService.DeleteBookAsync(id);
+        if (deletedBook == null) return NotFound();
+        return Ok(deletedBook);
+    }
+    
+    
+    
 }

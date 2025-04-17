481deb6c5b0b215b1cb30298cc808d3cb9ae7eff
Adelina Sharafutdinova <adelisha.dinov@gmail.com>
2025-04-17 12:06:41 +0300

    add: userDTO

---
+++ src/BookStore/BookStore.Api/Application/DTO/UserRequestDto.cs
@@ -0,0 +1,8 @@
+namespace BookStore.Api.Application.DTO;
+
+public class UserRequestDto
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ src/BookStore/BookStore.Api/Application/DTO/UserResponseDto.cs
@@ -0,0 +1,11 @@
+namespace BookStore.Api.Application;
+
+public class UserResponseDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public Guid CartId { get; set; }
+    public Guid FavoriteBooksId { get; set; }
+}

0ffcfa2d2e917ecb9aaeb1c47401bd3eba8bf36b
Adelina Sharafutdinova <adelisha.dinov@gmail.com>
2025-04-29 12:53:17 +0300

    убрала пока что юзера для добавки новой миграции бд книг

--- src/BookStore/BookStore.Domain/Entities/User.cs
+++ src/BookStore/BookStore.Domain/Entities/User.cs
@@ -2,13 +2,13 @@ using BookStore.Domain.Enums;
 
 namespace BookStore.Domain.Entities;
 
-public class User
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public UserRole Role { get; set; } = UserRole.Customer;
-    public Guid CartId { get; set; }
-    public Guid FavoriteBooksId { get; set; }
-}
+// public class User
+// {
+//     public Guid Id { get; set; }
+//     public string Name { get; set; }
+//     public string Email { get; set; }
+//     public string Password { get; set; }
+//     public UserRole Role { get; set; } = UserRole.Customer;
+//     public Guid CartId { get; set; }
+//     public Guid FavoriteBooksId { get; set; }
+// }
